12f4c166799171a973315bc9f9d1f7a590c25600
Krasimir Nikolov <krasimir.nikolov@accedia.com>
2024-12-09 18:26:25 +0200

    sdfasdf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
-    azurerm = {
+    azurerm = {     
       source  = "hashicorp/azurerm"
-      version = "4.13.0"
+      version = "4.11.0" 
     }
   }
 }
@@ -28,6 +28,26 @@ resource "azurerm_service_plan" "taskboardsp" {
   location            = azurerm_resource_group.taskboardrg.location
   os_type             = "Linux"
   sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "taskboardlwa" {
+  name                = "${var.app_service_name}${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.taskboardrg.name
+  location            = azurerm_resource_group.taskboardrg.location
+  service_plan_id     = azurerm_service_plan.taskboardsp.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "6.0"
+    }
+
+    always_on = false
+  }
+   connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.sqlserverkn.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.taskboarddb.name};User ID=${azurerm_mssql_server.sqlserverkn.administrator_login};Password=${azurerm_mssql_server.sqlserverkn.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
 }
 
 resource "azurerm_mssql_server" "sqlserverkn" {
@@ -56,30 +76,10 @@ resource "azurerm_mssql_firewall_rule" "sqlserverfr" {
   end_ip_address   = "0.0.0.0"
 }
 
-resource "azurerm_linux_web_app" "taskboardlwa" {
-  name                = "${var.app_service_name}${random_integer.ri.result}"
-  resource_group_name = azurerm_resource_group.taskboardrg.name
-  location            = azurerm_service_plan.taskboardsp.location
-  service_plan_id     = azurerm_service_plan.taskboardsp.id
-
-  site_config {
-    application_stack {
-      dotnet_version = "6.0"
-    }
-
-    always_on = false
-  }
-
-  connection_string {
-    name  = "DefaultConnection"
-    type  = "SQLAzure"
-    value = "Data Source=tcp:${azurerm_mssql_server.sqlserverkn.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.taskboarddb.name};User ID=${azurerm_mssql_server.sqlserverkn.administrator_login};Password=${azurerm_mssql_server.sqlserverkn.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
-  }
-}
-
 resource "azurerm_app_service_source_control" "taskboardassc" {
   app_id   = azurerm_linux_web_app.taskboardlwa.id
   repo_url = var.repo_url
   branch   = var.branch_name
+  use_manual_integration = true
 }
 
